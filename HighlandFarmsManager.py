d13f17b9b563cfecfe08ccaac49e0e98c758702e
adam-weiler <adam.weiler@yahoo.ca>
2019-07-23 22:15:25 -0400

    Starting new project.

---
+++ HighlandFarmsManager.py
@@ -0,0 +1,19 @@
+from farm import Farm
+import sys #Need to trigger the exit command.
+
+class HighlandFarmsManager:
+
+    def main_menu(self):
+        pass
+
+    def print_main_menu(self):
+        pass
+        
+        #Display Options
+        # 1 Add Field
+        # 2 Harvest crops
+        # 3 Check Status
+        #4 Relax
+        #5 Exit
+
+
---
+++ farm.py
@@ -0,0 +1,22 @@
+class Farm:
+    field = []
+
+    def __init__(self, crop, hectare):
+        self.crop = crop
+        self.hectare = hectare
+
+    def add_field():
+        pass #Add new field.
+
+    def harvest_crops():
+        pass #Collect food from all fields. 
+        #Record total
+        #Display
+
+    def check_status(): #Status of fields (size, type)
+        pass #How much food produced till now.
+
+    def relax(): #Provides lovely description of fields?
+        pass
+    
+    
