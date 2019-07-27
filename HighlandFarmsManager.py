1721803c2f14a3b26ffbe2a519234e3d8ad48dd9
adam-weiler <adam.weiler@yahoo.ca>
2019-07-27 13:44:39 -0400

    Adding create class method, and check status class method.

--- HighlandFarmsManager.py
+++ HighlandFarmsManager.py
@@ -1,19 +1,118 @@
-from farm import Farm
-import sys #Need to trigger the exit command.
+from farm import Field
+import sys #Needed to trigger the exit command.
 
 class HighlandFarmsManager:
 
     def main_menu(self):
-        pass
+        print('Starting up the Highland Farms Manager app')
+        while True:
+            print('\nWhat would you like to do?')
+            self.print_main_menu()
+            user_selected = 1
+            #user_selected = int(input())
+            self.call_option(user_selected)
+            break
+
+
 
     def print_main_menu(self):
+        print('[1] Add Field --> Adds a new field.')
+        # print('[2] Harvest crops --> Harvests crops, & adds to total harvested.')
+        # print('[3] Check Status')
+        # print('[4] Relax --> Provides lovely description of the fields.')
+        print('[5] Exit\n')
+
+
+    # def valid_input(self, user_input, expected_input, expected_input2=''):
+    #     while True:
+    #         print(user_input)
+    #         print(expected_input)
+    #         print(expected_input2)
+
+    #         if (user_input == expected_input) or (user_input == expected_input2):
+    #             print('Valid input.\n')
+    #             return user_input
+    #         else:
+    #             print('Invalid input.\n')
+    #             # break
+    #             user_input = input()
+
+
+    def call_option(self, user_selected):
+        if user_selected == 1:
+            self.add_field()
+        pass
+
+
+
+    def add_field(self):
+        print('What kind of field is it: corn or wheat?')
+        new_crop = 'corn'
+        # crop = input()
+        print('How large is the field in hectares?')
+        new_hectare = 50
+        # hectare = input()
+        Field.create(new_crop, new_hectare)
+
+        # return 'Added a {crop} field of {hectare} hectares!'
+
+    def harvest_crops(self):
+        #for crop in crops
+            #optional: print ('Harvesting {} food from a {} hectare corn field.')
+            #optional: print ('Harvesting {} food from a {} hectare corn field.')
+            #..etc
+        # return f'The farm has {} harvested food so far.'
         pass
         
-        #Display Options
-        # 1 Add Field
-        # 2 Harvest crops
-        # 3 Check Status
-        #4 Relax
-        #5 Exit
+
+    def check_status(self):
+        print('\nDisplay Info about the farm:')
+        Field.check_status()
 
 
+
+
+    def relax(self):
+        print('\nEnjoy the beauty of each one of your fields:')
+        Field.relax()
+        #optional: print ('{} hectares of tall green stalks rustling in the breeze fill your horizon.')
+        #optional: print ('The sun hangs low, casting an orange glow on a sea of {} hectares of wheat.')
+        pass
+
+
+    def exit(self):
+        print('Exiting...')
+        # exit()
+
+
+
+
+
+
+
+# HighlandFarmsManager.valid_input('corn', 'corn', 'wheat')
+
+# HighlandFarmsManager.valid_input('ducks', 'corn', 'wheat')    #validate_string
+
+# HighlandFarmsManager.valid_input(0, type(0))      #Maybe validate_number  ?
+
+
+our_hfm_app = HighlandFarmsManager()
+
+our_hfm_app.main_menu()
+our_hfm_app.print_main_menu()
+
+our_hfm_app.check_status()
+
+our_hfm_app.relax()
+
+
+
+
+
+#Stretch
+#Users enter bad input.
+#new field type - blueberries?
+#Pastures
+    #To store animals
+    #On harvest animals breed to make more animals.
--- farm.py
+++ farm.py
@@ -1,22 +1,35 @@
-class Farm:
-    field = []
+class Field:
+    fields = []
 
     def __init__(self, crop, hectare):
+        '''This method iniatlizes a new Field, with crop and hectare as attributes.'''
         self.crop = crop
         self.hectare = hectare
 
-    def add_field():
-        pass #Add new field.
+    def __str__(self):
+        return f'Your {self.crop.capitalize()} field is {self.hectare} hectares.'
 
-    def harvest_crops():
+    @classmethod
+    def create(cls, new_crop, new_hectare):
+        new_field = Field(new_crop, new_hectare)
+        cls.fields.append(new_field)
+        return(new_field)
+
+    def harvest_crops(self):
         pass #Collect food from all fields. 
         #Record total
         #Display
 
-    def check_status(): #Status of fields (size, type)
+    @classmethod
+    def check_status(cls): #Status of fields (size, type)
+        if len(cls.fields) > 0:
+            for field in cls.fields:
+                print (f'{field}')
+            return f'The farm has 27? harvested food so far.'
+        else:
+            print('You haven\'t harvested anything yet!')
         pass #How much food produced till now.
 
-    def relax(): #Provides lovely description of fields?
+    @classmethod
+    def relax(cls): #Provides lovely description of fields?
         pass
-    
-    
